715f1084a5b3c92101748699aadf8e3fa24934fb
mosescode1 <emosescode@gmail.com>
2024-04-17 09:23:20 +0100

    creating my database

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,8 +1,9 @@
--- Setting up my database
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 
-
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT ALL PRIVILEGES ON hbnb_dev_db TO  'hbnb_dev'@'localhost';
 
-GRANT SELECT ON performance_schema TO  'hbnb_dev'@'localhost';
+USE mysql; -- Switch to the mysql system database
+
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost' WITH GRANT OPTION;
+
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
