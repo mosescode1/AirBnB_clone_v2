f2d9d0360e484b97e93535fa137901ede50b391d
mosescode1 <emosescode@gmail.com>
2024-05-04 01:39:33 +0100

    all

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -5,7 +5,13 @@ sudo apt-get -y install nginx
 sudo service nginx start
 new_string="server_name _;\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}"
 string2=" root /data/web_static/current/;"
+# making dirctories 
+sudo mkdir -p /data/
+sudo mkdir -p /data/web_static/
+sudo mkdir -p /data/web_static/releases/
+sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test/
+# creating a fake profile
 echo "<html>
   <head>
   </head>
@@ -13,13 +19,9 @@ echo "<html>
     Holberton School
   </body>
 </html>" | sudo tee /data/web_static/releases/test/index.html
-if [ -L /data/web_static/current ];
-then
-        sudo rm -rf /data/web_static/current
-fi
-
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
+
 sudo sed -i "s#server_name _;#$new_string#"  /etc/nginx/sites-enabled/default
 sudo sed -i "s#root /var/www/html/;#$string2#"  /etc/nginx/sites-enabled/default
 sudo service nginx reload
