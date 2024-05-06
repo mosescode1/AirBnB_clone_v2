411277fcdbcee4798ccce332aef09f30ac27e822
mosescode1 <emosescode@gmail.com>
2024-05-06 10:06:24 +0100

    Redo packing

---
+++ 1-pack_web_static.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" A script that generates tgz archive from the contents """ 
+from fabric.api import local
+from datetime import datetime
+import os
+
+file_name = "web_static_{}.tgz".format(datetime.now().strftime("%Y%m%d%H%M"))
+
+def do_pack():
+    """ Function to generate tgz file"""
+    local("mkdir -p versions")
+    result = local(f"tar -czvf versions/{file_name} web_static")
+    path = os.path.abspath(f"versions/{file_name}")
+    size = os.path.getsize(path)
+    # print(path)
+    if result.failed:
+        return None
+    print(f"web_static packed: versions/{file_name} -> {size}Bytes")
+
