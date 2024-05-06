e2f3e9eef410febde9b1a7a8203ebb17f4767c02
mosescode1 <emosescode@gmail.com>
2024-05-06 15:18:06 +0100

    checking if it's all fixed now

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -4,12 +4,9 @@ from fabric.api import *
 import sys
 import os
 
-if len(sys.argv) > 1:
-    env.user = sys.argv[-1]
-    env.key_filename = sys.argv[-3]
-else:
-    print("Please provide a user argument.")
-    sys.exit(1)
+
+env.user = sys.argv[-1]
+env.key_filename = sys.argv[-3]
 
 env.hosts = ["107.23.92.1", "100.26.249.41"]
 
@@ -24,26 +21,26 @@ def deploy(archive_path):
     Returns:
         bool: True if the deployment is successful, False otherwise.
     """
-    if not os.path.isfile(archive_path):
-        print(f"The file {archive_path} does not exist.")
+    if not os.path.exists(archive_path):
         return False
 
     try:
         put(archive_path, "/tmp/")
-        file_name = os.path.basename(archive_path).replace(".tgz", "")
+        file_name = os.path.basename(archive_path).strip(".tgz")
+        # print(file_name)
         run(f"mkdir -p /data/web_static/releases/{file_name}/")
-        run(f"tar -xzf /tmp/{file_name}.tgz -C /data/web_static/releases/{file_name}/")
+        run(f"tar -xzf /tmp/{file_name}.tgz --directory=/data/web_static/releases/{file_name}/")
         run(f"rm /tmp/{file_name}.tgz")
         run(
-            f"mv /data/web_static/releases/{file_name}/web_static/* /data/web_static/releases/{file_name}/"
+            f"mv --force /data/web_static/releases/{file_name}/web_static/* /data/web_static/releases/{file_name}/"
         )
         run(f"rm -rf /data/web_static/releases/{file_name}/web_static")
         run("rm -rf /data/web_static/current")
-        run(f"ln -s /data/web_static/releases/{file_name}/ /data/web_static/current")
+        run(
+            f"ln -s /data/web_static/releases/{file_name}/ /data/web_static/current")
         print("New version deployed")
         return True
     except Exception as e:
-        print(f"An error occurred: {e}")
         return False
 
 
