d79d970b677c2654d838d8dd599cd221e61179d7
mosescode1 <emosescode@gmail.com>
2024-05-16 16:56:30 +0100

    web flask

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+from flask import Flask
+#from web_flask import app
+
+app = Flask(__name__)
+
+
+
+@app.route("/", strict_slashes=False)
+def home():
+    return "Hello HBNB!"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
